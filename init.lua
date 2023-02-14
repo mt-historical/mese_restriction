87598d1740efeb5e87a0b19002eb8a0254affb1c
unknown <24964441+wsor4035@users.noreply.github.com>
2023-02-13 21:01:31 -0500

    update mod to run after all mods are loaded, and make sure item is registered

--- init.lua
+++ init.lua
@@ -30,18 +30,22 @@ local mese_list = {
 	"mesecons_gates:xor_off",
 }
 
-for mesecount = 1, #mese_list do
-	minetest.override_item(mese_list[mesecount], {
-		on_place = function(itemstack, placer, pointed_thing)
-			local can_mess = minetest.check_player_privs(placer.get_player_name(placer), {mesemaker=true})
-			if not can_mess then
-				minetest.chat_send_player(placer:get_player_name(), "You're not allowed to use this.")
-				return
-			end
-			return minetest.item_place(itemstack, placer, pointed_thing)
-		end	
-	})
-end
+minetest.register_on_mods_loaded(function()
+	for mesecount = 1, #mese_list do
+		if minetest.registered_items[mese_list[mesecount]] then
+			minetest.override_item(mese_list[mesecount], {
+				on_place = function(itemstack, placer, pointed_thing)
+					local can_mess = minetest.check_player_privs(placer.get_player_name(placer), {mesemaker=true})
+					if not can_mess then
+						minetest.chat_send_player(placer:get_player_name(), "You're not allowed to use this.")
+						return
+					end
+					return minetest.item_place(itemstack, placer, pointed_thing)
+				end	
+			})
+		end
+	end
+end)
 
 
 
